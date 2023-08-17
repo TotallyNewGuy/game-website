883fffee487b61b4ff221364ff4987ce9e9df71b
zhihaoTCXR <zhihaol.tcxr@gmail.com>
2023-08-17 15:50:13 +0800

    fetch the genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
@@ -17,10 +18,16 @@ const GameGrid = () => {
       >
         {isLoading &&
           skeletons.map((skeleton) => {
-            return <GameCardSkeleton key={skeleton}></GameCardSkeleton>;
+            return (
+              <GameCardContainer>
+                <GameCardSkeleton key={skeleton}></GameCardSkeleton>
+              </GameCardContainer>
+            );
           })}
         {games.map((game) => (
-          <GameCard key={game.id} game={game}></GameCard>
+          <GameCardContainer>
+            <GameCard key={game.id} game={game}></GameCard>
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
